912ca94c87c99f4cf6092ae65a7e41e75bcc1f46
Warlock Audio <soheil.hooshmandi.sh@gmail.com>
2026-08-28 20:22:42 +0200

    Add MONOLITH amp and cabinet rack (models, IRs, parameters).

---
+++ artifacts/WarlockMonolith-Setup/plugin/WarlockAmpCabParameters.h
@@ -0,0 +1,22 @@
+#pragma once
+namespace warlock
+{
+inline constexpr const char* kParamAmpOn = "AMP_ON";
+inline constexpr const char* kParamAmpModel = "AMP_MODEL";
+inline constexpr const char* kParamAmpInput = "AMP_INPUT";
+inline constexpr const char* kParamAmpDrive = "AMP_DRIVE";
+inline constexpr const char* kParamAmpBass = "AMP_BASS";
+inline constexpr const char* kParamAmpMid = "AMP_MID";
+inline constexpr const char* kParamAmpTreble = "AMP_TREBLE";
+inline constexpr const char* kParamAmpPresence = "AMP_PRESENCE";
+inline constexpr const char* kParamAmpSag = "AMP_SAG";
+inline constexpr const char* kParamAmpMaster = "AMP_MASTER";
+inline constexpr const char* kParamCabOn = "CAB_ON";
+inline constexpr const char* kParamCabModel = "CAB_MODEL";
+inline constexpr const char* kParamCabMic = "CAB_MIC";
+inline constexpr const char* kParamCabMix = "CAB_MIX";
+inline constexpr const char* kParamCabLowCut = "CAB_LOWCUT";
+inline constexpr const char* kParamCabHighCut = "CAB_HIGHCUT";
+inline constexpr const char* kParamCabLoFi = "CAB_LOFI";
+inline constexpr const char* kParamCabStereo = "CAB_STEREO";
+}
