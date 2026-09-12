6d0be3e8ab2af7f44dd83b2f015eee6ea4003f8b
Warlock Audio <soheil.hooshmandi.sh@gmail.com>
2026-09-12 10:43:29 +0200

    Use non-nested account products route convention

--- src/routes/account.products.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { AccountShell } from "@/components/account/account-shell";
-
-export const Route = createFileRoute("/account/products")({
-  component: () => <AccountShell section="products" />,
-});
