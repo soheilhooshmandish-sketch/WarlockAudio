7e8c0639c0e5da57290ef690db0cf5c7dc7e9c20
Warlock Audio <soheil.hooshmandi.sh@gmail.com>
2026-09-12 10:43:35 +0200

    Use non-nested account builds route convention

--- src/routes/account.builds.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { AccountShell } from "@/components/account/account-shell";
-
-export const Route = createFileRoute("/account/builds")({
-  component: () => <AccountShell section="builds" />,
-});
