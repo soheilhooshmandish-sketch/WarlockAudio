d05e46cd72cd7f23b55dad6b79e77ffc6b82808f
Warlock Audio <soheil.hooshmandi.sh@gmail.com>
2026-09-12 10:43:50 +0200

    Use non-nested account billing route convention

--- src/routes/account.billing.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { AccountShell } from "@/components/account/account-shell";
-
-export const Route = createFileRoute("/account/billing")({
-  component: () => <AccountShell section="billing" />,
-});
